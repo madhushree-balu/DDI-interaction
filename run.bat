52a048cf3f1d609b1f414d84bdaa8267e0bf356f
Parvat R <vidyutparvat2@gmail.com>
2026-03-29 23:58:52 +0530

    fixed run

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-python -m venv venv
-venv\Scripts\activate
 pip install -r requirements.txt
 curl -O -L http://py-tesseract-zip-here.s3-website.ap-south-1.amazonaws.com/Tesseract-OCR.zip
 tar -xf Tesseract-OCR.zip
